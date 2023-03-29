bd0749d1a83ab9f517ae9ff046109873b60c01d9
NelsonCC <nelsoncc1812@gmail.com>
2023-03-29 12:27:47 +0100

    Loging with styles

--- src/app/templates/forms/forms.module.ts
+++ src/app/templates/forms/forms.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.page';
+import { LoginComponent } from './login/login.component';
 import { FormsRoutingModule } from './forms-routing.module';
 
 
