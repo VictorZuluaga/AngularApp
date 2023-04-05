c62bd2bab4e5f516c11f28e32bbd6736d0234aa2
Victor Zuluaga <vj_626@hotmail.com>
2023-04-05 11:58:48 +0100

    ajustado el responsive del header a angular

--- src/app/sharedComponents/header/header.component.ts
+++ src/app/sharedComponents/header/header.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Renderer2, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+
+export class HeaderComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('header') header!: ElementRef;
+
+  constructor(private renderer2: Renderer2) { }
+
+  ngOnInit(): void {
+    console.log("me ejecuto al inicio");
+  }
+
+  ngAfterViewInit(): void {
+
+    const header = this.header.nativeElement;
+    //cuatro elementos dentro del header
+    const hamburger = document.querySelector(".header .hamburger");
+    const busqueda = document.querySelector(".header .busqueda");
+    const navLinks = document.querySelector(".header .nav-links");
+    const otherLinks = document.querySelector(".header .other-links");
+
+    this.renderer2.listen(hamburger, 'click', () => {
+      hamburger!.classList.toggle("active");
+      busqueda!.classList.toggle("active");
+      navLinks!.classList.toggle("active");
+      otherLinks!.classList.toggle("active");
+    });
+  }
+
+
 
 }
