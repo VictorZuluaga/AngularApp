f031516078c9f217aef3792bf3fb69ae7b85428f
Victor Zuluaga <vj_626@hotmail.com>
2023-04-05 17:35:41 +0100

    styles de los hijos aplicados desde el padre

--- src/app/templates/main-home/articulos-principales/articulos-principales.component.ts
+++ src/app/templates/main-home/articulos-principales/articulos-principales.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-articulos-principales',
   templateUrl: './articulos-principales.component.html',
-  styleUrls: ['./articulos-principales.component.css']
+  styleUrls: ['./articulos-principales.component.css','../main-home.component.css']
 })
 export class ArticulosPrincipalesComponent {
 
--- src/app/templates/main-home/articulos-secundarios/articulos-secundarios.component.ts
+++ src/app/templates/main-home/articulos-secundarios/articulos-secundarios.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-articulos-secundarios',
   templateUrl: './articulos-secundarios.component.html',
-  styleUrls: ['./articulos-secundarios.component.css']
+  styleUrls: ['./articulos-secundarios.component.css','../main-home.component.css']
 })
 export class ArticulosSecundariosComponent {
 
