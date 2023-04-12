414e60a7f68d77b46de7e0e441d86a233c402575
Victor Zuluaga <vj_626@hotmail.com>
2023-04-12 11:51:04 +0100

    formulario de subscribe incluido

---
+++ src/app/templates/forms/subscribe-form/subscribe-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribe-form',
+  templateUrl: './subscribe-form.component.html',
+  styleUrls: ['../forms.css',
+    './subscribe-form.component.css']
+})
+export class SubscribeFormComponent {
+
+}
