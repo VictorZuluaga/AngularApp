31f1eabede4b4acc2b74165cbfbc59b94cd4b870
Victor Zuluaga <vj_626@hotmail.com>
2023-04-12 12:42:35 +0100

    añadido main home como modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,11 @@ import { FooterComponent } from './sharedComponents/footer/footer.component';
 import { FormsRoutingModule } from './templates/forms/forms-routing.module';
 import { SubscribeComponent } from './templates/subscribe/subscribe.component';
 import { HeaderComponent } from './sharedComponents/header/header.component';
-import { MainHomeComponent } from './templates/main-home/main-home.component';
-import { ArticulosPrincipalesComponent } from './templates/main-home/articulos-principales/articulos-principales.component';
-import { ArticulosSecundariosComponent } from './templates/main-home/articulos-secundarios/articulos-secundarios.component';
-import { ListaVideosComponent } from './templates/main-home/lista-videos/lista-videos.component';
 import { PaginaArticleComponent } from './templates/pagina-article/pagina-article.component';
 import { ArticleComponent } from './templates/pagina-article/article/article.component';
 import { CajaComentariosComponent } from './templates/pagina-article/caja-comentarios/caja-comentarios.component';
 import { PolicyModule } from './templates/policy/policy.module';
+import { MainHomeModule } from './templates/main-home/main-home.module';
 
 @NgModule({
   declarations: [
@@ -24,10 +21,6 @@ import { PolicyModule } from './templates/policy/policy.module';
     SubscribeComponent,
     AppComponent,
     HeaderComponent,
-    MainHomeComponent,
-    ArticulosPrincipalesComponent,
-    ArticulosSecundariosComponent,
-    ListaVideosComponent,
     PaginaArticleComponent,
     ArticleComponent,
     CajaComentariosComponent
@@ -38,7 +31,8 @@ import { PolicyModule } from './templates/policy/policy.module';
     FormsRoutingModule,
     AppRoutingModule,
     HttpClientModule,
-    PolicyModule
+    PolicyModule,
+    MainHomeModule
   ],
   providers: [],
   bootstrap: [ AppComponent ]
