3f598169654dff1cccc9655d7a4c701ed884f786
NelsonCC <nelsoncc1812@gmail.com>
2023-04-17 09:12:57 +0100

    doing pruebas

--- src/app/tests/tests.module.ts
+++ src/app/tests/tests.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestComponent } from './test/test.component';
 import { TestsRoutingModule } from './tests-routing.module';
+import { CalledComponent } from './called/called.component';
 
 
 
 @NgModule({
   declarations: [
-    TestComponent
+    TestComponent,
+    CalledComponent
   ],
   imports: [
     CommonModule, TestsRoutingModule
