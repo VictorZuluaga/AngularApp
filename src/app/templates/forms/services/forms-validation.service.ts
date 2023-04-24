55b206104c49777bbf8957e3a1febe45bf1585e5
PedroRS9 <pedroglv98@gmail.com>
2023-04-24 08:55:53 +0100

    Validación de forms: Signup y Login

---
+++ src/app/templates/forms/services/forms-validation.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormsValidationService {
+
+  constructor() { }
+
+  isEmail: (email: string) => boolean = (email) => 
+  {    
+    console.log("email = " + email);
+    console.log(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email))
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  .test(email)
+  }
+
+}
--- src/app/templates/forms/signup/signup.component.ts
+++ src/app/templates/forms/signup/signup.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsValidationService } from '../services/forms-validation.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['../forms.css', './signup.component.css']
+  styleUrls: ['../forms.css', './signup.component.css'],
+  providers: [FormsValidationService]
 })
-export class SignupComponent {
-  constructor() { }
-
-  email: String = ""
+export class SignupComponent {  
+  constructor(protected validatorService: FormsValidationService) { }
+  
+  email: string = ""
   password: String = ""
   repeat_password: String = ""
   showError: Boolean = false
 
-  btnHandler: () => void = () => console.log(`email: ${this.email}, password: ${this.password} password-repeat: ${this.repeat_password}`, event?.target)
+
+  btnHandler: () => void = () => console.log(`email: ${this.email}, password: ${this.password} password-repeat: ${this.repeat_password}`, event?.target)      
 }
