8a4b1c6a639361bafc6c9ae8adf33842f9ef16eb
NelsonCC <nelsoncc1812@gmail.com>
2023-04-26 10:50:45 +0100

    merged victor

--- src/app/templates/forms/signup/signup.component.ts
+++ src/app/templates/forms/signup/signup.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
-import { FormsValidationService } from '../services/forms-validation.service';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['../forms.css', './signup.component.css'],
-  providers: [FormsValidationService]
+  styleUrls: [ '../forms.css', './signup.component.css' ],
+  providers: []
 })
-export class SignupComponent {  
-  constructor(protected validatorService: FormsValidationService) { }
-  
+export class SignupComponent {
+  constructor() { }
+
   email: string = ""
   password: String = ""
   repeat_password: String = ""
   showError: Boolean = false
 
-
-  btnHandler: () => void = () => console.log(`email: ${this.email}, password: ${this.password} password-repeat: ${this.repeat_password}`, event?.target)      
+  emailHandler: (any) => void = (e) =>
+    btnHandler: () => void = () => console.log(`email: ${this.email}, password: ${this.password} password-repeat: ${this.repeat_password}`, event?.target)
 }
